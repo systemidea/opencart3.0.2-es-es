648a991d27280e6b3a5b3a06391ac3b3d53325ed
Carlos Lozada <haxcop@yahoo.com>
2019-01-13 13:39:42 +0000

    update product page
    traduccion del texto al Espanol

--- catalog/language/es-es/product/product.php
+++ catalog/language/es-es/product/product.php
@@ -1,50 +1,50 @@
 <?php
 // Text
 $_['text_search']              = 'Buscar';
-$_['text_brand']               = 'Brand';
-$_['text_manufacturer']        = 'Brand:';
-$_['text_model']               = 'Product Code:';
-$_['text_reward']              = 'Reward Points:';
-$_['text_points']              = 'Precio en Puntos de Regalo:';
-$_['text_stock']               = 'Availability:';
-$_['text_instock']             = 'In Stock';
-$_['text_tax']                 = 'Ex Tax:';
-$_['text_discount']            = ' or more ';
-$_['text_option']              = 'Available Options';
-$_['text_minimum']             = 'This product has a minimum quantity of %s';
-$_['text_reviews']             = '%s reviews';
-$_['text_write']               = 'Write a review';
-$_['text_login']               = 'Please <a href="%s">login</a> or <a href="%s">register</a> to review';
-$_['text_no_reviews']          = 'There are no reviews for this product.';
-$_['text_note']                = '<span class="text-danger">Note:</span> HTML is not translated.';
-$_['text_success']             = 'Thank you for your review. It has been submitted to the webmaster for approval.';
-$_['text_related']             = 'Related Products';
-$_['text_tags']                = 'Tags:';
-$_['text_error']               = 'Product not found.';
-$_['text_payment_recurring']   = 'Payment Profile';
+$_['text_brand']               = 'Marca';
+$_['text_manufacturer']        = 'Fabricante:';
+$_['text_model']               = 'Modelo:';
+$_['text_reward']              = 'Recompensa:';
+$_['text_points']              = 'Puntos:';
+$_['text_stock']               = 'Disponibilidad:';
+$_['text_instock']             = 'Disponible';
+$_['text_tax']                 = 'impuesto:';
+$_['text_discount']            = 'Descuento';
+$_['text_option']              = 'Opciones';
+$_['text_minimum']             = 'La cantidad minima para este producto es %s';
+$_['text_reviews']             = '%s Evaluacion';
+$_['text_write']               = 'Escribe tu evaluación';
+$_['text_login']               = 'Favor <a href="%s">Ingresa</a> o <a href="%s">Registrate</a> para Evaluar';
+$_['text_no_reviews']          = 'No hay Evluaciones para este producto.';
+$_['text_note']                = '<span class="text-danger">Note:</span> HTML no es traducido.';
+$_['text_success']             = 'Gracias por su Evaluacion. Sera puesto en revision por el Administrador.';
+$_['text_related']             = 'Productos relacionados';
+$_['text_tags']                = 'Etiquetas:';
+$_['text_error']               = 'Producto no encontrado.';
+$_['text_payment_recurring']   = 'Perfil de Pago';
 $_['text_trial_description']   = '%s every %d %s(s) for %d payment(s) then';
 $_['text_payment_description'] = '%s every %d %s(s) for %d payment(s)';
 $_['text_payment_cancel']      = '%s every %d %s(s) until canceled';
-$_['text_day']                 = 'day';
-$_['text_week']                = 'week';
-$_['text_semi_month']          = 'half-month';
-$_['text_month']               = 'month';
-$_['text_year']                = 'year';
+$_['text_day']                 = 'Dia';
+$_['text_week']                = 'Semana';
+$_['text_semi_month']          = 'medio-mes';
+$_['text_month']               = 'mes';
+$_['text_year']                = 'año';
 
 // Entry
 $_['entry_qty']                = 'Cantidad';
 $_['entry_name']               = 'Nombre';
-$_['entry_review']             = 'Your Review';
-$_['entry_rating']             = 'Rating';
-$_['entry_good']               = 'Good';
-$_['entry_bad']                = 'Bad';
+$_['entry_review']             = 'Tu evaluación';
+$_['entry_rating']             = 'clasificación';
+$_['entry_good']               = 'Bueno';
+$_['entry_bad']                = 'Malo';
 
 // Tabs
 $_['tab_description']          = 'Descripción';
-$_['tab_attribute']            = 'Specification';
+$_['tab_attribute']            = 'Atributo';
 $_['tab_review']               = 'Comentarios (%s)';
 
 // Error
-$_['error_name']               = 'Error: El Nombre de Revisión debe contener entre 3 y 25 caracteres.';
-$_['error_text']               = 'Error: El Texto de Revisión debe contener entre 25 y 1000 caracteres.';
-$_['error_rating']             = 'Error: Please select a review rating.';
+$_['error_name']               = 'Error: El Nombre de evaluación debe contener entre 3 y 25 caracteres.';
+$_['error_text']               = 'Error: El Texto de evaluación debe contener entre 25 y 1000 caracteres.';
+$_['error_rating']             = 'Error: Por favor seleccione la clasificación para la evaluación.';
